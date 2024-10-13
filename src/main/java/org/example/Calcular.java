13ce9c01c6e5aaec5e8cd036e5b8a9ae90c3d49f
eduardo <eduardodeassis@gmail.com>
2024-10-13 00:07:30 -0300

    Inicializando o repositorio

---
+++ src/main/java/org/example/Calcular.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Calcular {
+
+    /**
+     * <h1>PROPOSTA DA ATIVIDADE</h1>
+     * <p>Em uma escola, foi estabelecido que o nosso professor vai poder colocar quantas vezes ele quiser uma
+     * nota de um aluno e vai ser necessário calcularmos a nota média que foi obtida
+     * e mostrar se o aluno ao longo do ano conseguiu bater a média que é de 6.</p>
+     *
+     * <h2>REQUISITOS QUE VÃO SER AVALIADOS</h>
+     * <ul>
+     *     <li>O sistema deve ter uma forma do professor digitar as notas dos alunos.</li>
+     *     <li>Todas essas notas vão ser armazenadas em uma parte no código.</li>
+     *     <li>Calcular a média de todas essas notas.</li>
+     *     <li>Mostra se ele foi aprovado ou não.</li>
+     * </ul>
+     */
+    public static void calcularMedia() {
+        
+    }
+
+    public static void main(String[] args) {
+        calcularMedia();
+    }
+}
